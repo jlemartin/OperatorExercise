a4c3323091dbf29e535e5e8dbab73227af5ee5ed
John Martin <jlemartin7@gmail.com>
2020-10-26 11:43:20 -0500

    moved method outside of Main method, and other refinements

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -23,22 +23,16 @@ namespace OperatorExercise
             Console.WriteLine($"The product of {a} and {b} is {multiplication}.");
             Console.WriteLine($"{a} divided by {b} is {division}.");
             Console.WriteLine($"The remainder when {a} is divided by {b} is {remainder}.");
-            Console.WriteLine($"Thus, {a} / {b} is {division} with a remainder of {remainder}.");
+            Console.WriteLine($"Thus, {a}/{b} is {division} with a remainder of {remainder}.");
             Console.WriteLine();
 
             // Part #2
             Console.WriteLine("Part 2 of Exercise:");
 
-           static double AreaOfCircle(double r)
-            {
-                double pi = 3.14159;
-                double area = pi * (r * r);
-                return area;
-            }
-
             Console.WriteLine("What is the radius of the circle?");
             var radius = double.Parse(Console.ReadLine());
 
+            // calling the method AreaOfCircle
             double result = AreaOfCircle(radius);
 
             Console.WriteLine($"The area of the circle with radius of {radius} is {result}.");
@@ -49,11 +43,19 @@ namespace OperatorExercise
             var i = 3;
             var j = 4;
             var k = ++i * j++;
-            Console.WriteLine("In this scenario, 3 is incremented and then used.");
-            Console.WriteLine($"Then 4 is used, then incremented.");
-            Console.WriteLine($"Thus, 3 becomes 4 and multiplies 4");
+            // In this scenario, 3 is incremented and then used.
+            // Then 4 is used, then incremented after
+            // Thus, 3 becomes 4 and multiplies 4
             Console.WriteLine($"The result is {k}.");
 
+        }
+
+        // Method to calculate the area of the circle, given a radius of r
+        public static double AreaOfCircle(double r)
+        {
+            double pi = Math.PI;
+            double area = pi * (r * r);
+            return area;
         }
     }
 }
