c6ae4b4277f5c41c0266ab3178fdcad19d0b7e7c
John Martin <jlemartin7@gmail.com>
2020-10-26 10:27:03 -0500

    Part #1 of exercise

---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace OperatorExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Part #1
+
+            int a = 17;
+            int b = 4;
+
+            int addition = a + b;
+            int subtraction = a - b;
+            int multiplication = a * b;
+            int division = a / b;
+            int remainder = a % b;
+
+            Console.WriteLine($"The sum of {a} and {b} is {addition}.");
+            Console.WriteLine($"The difference of {a} and {b} is {subtraction}.");
+            Console.WriteLine($"The product of {a} and {b} is {multiplication}.");
+            Console.WriteLine($"{a} divided by {b} is {division}.");
+            Console.WriteLine($"The remainder when {a} is divided by {b} is {remainder}.");
+            Console.WriteLine($"Thus, {a} / {b} is {division} with a remainder of {remainder}.");
+
+
+        }
+    }
+}
